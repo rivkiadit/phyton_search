1c34b4ab8e797ed65208620a7a1dc0f0d9bcc0bb
rivkiadit <133620726+rivkiadit@users.noreply.github.com>
2023-05-22 11:21:51 +0700

    Add files via upload

---
+++ sequential_search_max.py
@@ -0,0 +1,10 @@
+def squential_search_max(data):
+    max_index = 0
+    for i in range(1, len(data)):
+        if data[i] > data[max_index]:
+            max_index = 1
+    return max_index
+
+my_list = [5, 9, 3, 2, 8, 7]
+max_index = squential_search_max(my_list)
+print(f"indeks elemen maksimum adalah {max_index}")
